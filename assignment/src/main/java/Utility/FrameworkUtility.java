664fb93e45c61b210c3450c4c2d3ab5f0888264e
Sunny123hpe <44165515+Sunny123hpe@users.noreply.github.com>
2020-02-07 20:34:17 +0530

    Update FrameworkUtility.java

--- assignment/src/main/java/Utility/FrameworkUtility.java
+++ assignment/src/main/java/Utility/FrameworkUtility.java
@@ -59,12 +59,21 @@ public class FrameworkUtility {
 	 
 	public void initilizeLocalDriver() throws Exception {
 
+		Map<String, Object> prefs = new HashMap<String, Object>();
+        
+        // Set the notification setting it will override the default setting
+		prefs.put("profile.default_content_setting_values.notifications", 2);
 
+        // Create object of ChromeOption class
+		ChromeOptions options = new ChromeOptions();
+
+        // Set the experimental option
+		options.setExperimentalOption("prefs", prefs);
 		
 		System.setProperty("webdriver.chrome.driver", Constants.driverPath);
 
 	
-		driver = new ChromeDriver();
+		driver = new ChromeDriver(options);
 
 		
 		driver.manage().window().maximize();
